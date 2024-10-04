f8232cf15c3ec448ae9faecb5e784d56a530f32a
Adam Southwick <adamsouthwick100@gmail.com>
2024-10-04 23:07:17 +0100

    added basic post function

--- emailService/app.py
+++ emailService/app.py
@@ -1,6 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def requestEmail():
+    data = request.get_json()
+    
+    if 'email' not in data or 'time' not in data:
+        return jsonify({'error': 'Email and time fields are required'}), 400
+    
+    email = data['email']
+    time = data['time']
+
+    # Add mailgun here
+
+    return jsonify({
+        'message': 'Data received successfully',
+        'email': email,
+        'time': time
+    }), 200
+    
 
 
 if __name__ == "__main__":
