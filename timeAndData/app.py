8cf27f521879fe09d911ece9b26db1fdd6fd6219
Adam Southwick <adamsouthwick100@gmail.com>
2024-10-05 13:19:23 +0100

    added cors policies and gunicorn

--- timeAndData/app.py
+++ timeAndData/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=False)
+    # CORS(app, origins=["https://landsatcheck.co"])
