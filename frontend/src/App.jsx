2cc18b05dbcadf75f13ad79c4761c94527870942
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2024-10-05 10:33:46 +0100

    positioning of divs fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,42 +2,37 @@
 import React from "react";
 import MapboxComponent from "./components/mapbox/MapboxComponent";
 import Toggle from "./components/StateToggle/Toggle";
-import './App.css';
-import 'mapbox-gl/dist/mapbox-gl.css'; 
+import "./App.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 // import './components/mapbox/mapbox.css';
-import EmailBoxComponent from './components/emailBox/emailBoxComponent';
+import EmailBoxComponent from "./components/emailBox/emailBoxComponent";
 
 function App() {
-
   const handleToggleChange = (selection) => {
-        
-        // Hide the div that isn't selected
-        const liveDiv = document.getElementById('Live');
-        const futureDiv = document.getElementById('Future');
+    // Hide the div that isn't selected
+    const liveDiv = document.getElementById("Live");
+    const futureDiv = document.getElementById("Future");
 
-        if (selection === 'Live') {
-            liveDiv.style.display = 'block';
-            futureDiv.style.display = 'none';
-        } else {
-            liveDiv.style.display = 'none';
-            futureDiv.style.display = 'block';
-        }
-  }
+    if (selection === "Live") {
+      liveDiv.style.display = "block";
+      futureDiv.style.display = "none";
+    } else {
+      liveDiv.style.display = "none";
+      futureDiv.style.display = "block";
+    }
+  };
 
   return (
     <>
-      <div>
+      <div id="toggleContainer">
         <Toggle handleToggleChange={handleToggleChange} />
-        <div id="Live">
-
-        </div>
-        <div id="Future">
+        <div id="Live"></div>
+        <div id="Future" style={{ display: "none" }}>
           <EmailBoxComponent />
-
         </div>
       </div>
-      <div>
-          <MapboxComponent />
+      <div id="mapContainer">
+        <MapboxComponent />
       </div>
     </>
   );
