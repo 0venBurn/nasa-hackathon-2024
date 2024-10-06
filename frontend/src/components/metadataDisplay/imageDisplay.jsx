f63d2e1c3da168f744c2abb9061965bdcdaa67ac
t-acheson <taraacheson@gmail.com>
2024-10-06 11:32:33 +0100

    boilerplate for dummy image ready to go

---
+++ frontend/src/components/metadataDisplay/imageDisplay.jsx
@@ -0,0 +1,11 @@
+import dummyGraphs from './assets/images/dummyGraph.jpg';
+
+function MyComponent() {
+  return (
+    <div>
+      <img src={dummyGraphs} alt="Description" />
+    </div>
+  );
+}
+
+export default MyComponent;
